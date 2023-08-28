da637616625635b2ef0dbf7b7fc7ea6c461a72a7
leonardos4enz <leonardorsaenz@gmail.com>
2023-08-28 07:21:52 -0600

    v-10

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    AOS.init();
     loadComponent('components/navbar.html', 'navbar-placeholder');
     loadComponent('components/starsbg.html', 'starsbg-placeholder');
 });
@@ -13,3 +14,34 @@ function loadComponent(url, placeholderId) {
     xhr.open('GET', url, true);
     xhr.send();
 }
+
+
+//Handwritting
+var vara = new Vara(
+    "#welcome", "https://cdn.jsdelivr.net/npm/vara@1.4.0/fonts/Pacifico/PacificoSLO.json", [
+    {
+        text: "Bienvenido/a",
+        fontSize: 48,
+        strokeWidth: 0.5,
+        color: "white",
+        id: "",
+        duration: 3000,
+        textAlign: "center",
+        autoAnimation: true,
+        queued: true,
+        delay: 0,
+        letterSpacing: 0
+    }
+    ], {
+    fontSize: 24,
+    strokeWidth: 0.5,
+    duration: 2000,
+    textAlign: "left",
+    autoAnimation: true,
+    queued: true,
+    letterSpacing: 0
+    }
+);
+
+
+vara.ready(function() {});
