30324e7d4a98aeecb698e21e204bf225b1b8a486
leonardos4enz <leonardorsaenz@gmail.com>
2023-10-02 01:00:31 -0600

    tesla scroll

--- index.js
+++ index.js
@@ -15,33 +15,3 @@ function loadComponent(url, placeholderId) {
     xhr.send();
 }
 
-
-//Handwritting
-var vara = new Vara(
-    "#welcome", "https://cdn.jsdelivr.net/npm/vara@1.4.0/fonts/Pacifico/PacificoSLO.json", [
-    {
-        text: "Bienvenido/a",
-        fontSize: 48,
-        strokeWidth: 0.5,
-        color: "white",
-        id: "",
-        duration: 3000,
-        textAlign: "center",
-        autoAnimation: true,
-        queued: true,
-        delay: 0,
-        letterSpacing: 0
-    }
-    ], {
-    fontSize: 24,
-    strokeWidth: 0.5,
-    duration: 2000,
-    textAlign: "left",
-    autoAnimation: true,
-    queued: true,
-    letterSpacing: 0
-    }
-);
-
-
-vara.ready(function() {});
